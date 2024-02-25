80a58901b44ae89f7597cd4a703e715630c17828
Zakaria <ahmad192450@gmail.com>
2024-02-25 23:43:06 +0300

    fix some problems

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
-let awesome = document.querySelector(".awesome");
-let num = document.querySelectorAll(".awesome .num");
+let skill = document.querySelector(".our-skills");
+let spans = document.querySelectorAll(".our-skills .skill span");
+let counts = document.querySelectorAll(".our-skills .skill .count");
+let started1 = false;
 
-window.onscroll = function () {
-  if (window.scrollY > 200) {
-    console.log("hello");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > skill.offsetTop - 200) {
+    if (!started1) {
+      spans.forEach((span) => startFill(span));
+      counts.forEach((count) => startCoun(count));
+    }
+    started1 = true;
   }
-};
+});
+
+function startFill(el) {
+  let width = el.dataset.skill;
+  el.style.width = `${width}%`;
+}
+
+function startCoun(el) {
+  let count = el.dataset.skill;
+  let int = setInterval(() => {
+    el.innerText++;
+    if (el.innerText == count) {
+      clearInterval(int);
+    }
+  }, 1500 / count);
+}
+
+let awesome = document.querySelector(".awesome");
+let nums = document.querySelectorAll(".awesome .num");
+let started2 = false;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > awesome.offsetTop - 200) {
+    if (!started2) {
+      nums.forEach((num) => startCount(num));
+    }
+    started2 = true;
+  }
+});
+
+function startCount(el) {
+  let goal = el.dataset.awe;
+  let count = setInterval(() => {
+    el.innerText++;
+    if (el.innerText == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
